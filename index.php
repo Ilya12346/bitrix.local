5e6103ff7b3bdbfd1e2b6ac970946f24b8750b72
Ilya12346 <160493090+Ilya12346@users.noreply.github.com>
2024-02-29 18:00:24 +0300

    all works
    all works

--- index.php
+++ index.php
@@ -3,76 +3,65 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("1С-Битрикс: Управление сайтом");
 ?>
 
-<? $APPLICATION->IncludeComponent(
-  "bitrix:news.list",
-  "template2",
-  array(
-    "COMPONENT_TEMPLATE" => "template2",
-    "IBLOCK_TYPE" => "slider",
-    "IBLOCK_ID" => "1",
-    "NEWS_COUNT" => "20",
-    "SORT_BY1" => "SORT",
-    "SORT_ORDER1" => "ASC",
-    "SORT_BY2" => "SORT",
-    "SORT_ORDER2" => "ASC",
-    "FILTER_NAME" => "",
-    "FIELD_CODE" => array(
-      0 => "ID",
-      1 => "NAME",
-      2 => "PREVIEW_TEXT",
-      3 => "PREVIEW_PICTURE",
-      4 => "DETAIL_PICTURE",
-      5 => "",
-    ),
-    "PROPERTY_CODE" => array(
-      0 => "",
-      1 => "label",
-      2 => "url",
-    ),
-    "CHECK_DATES" => "N",
-    "DETAIL_URL" => "",
-    "AJAX_MODE" => "N",
-    "AJAX_OPTION_JUMP" => "N",
-    "AJAX_OPTION_STYLE" => "Y",
-    "AJAX_OPTION_HISTORY" => "N",
-    "AJAX_OPTION_ADDITIONAL" => "",
-    "CACHE_TYPE" => "A",
-    "CACHE_TIME" => "36000000",
-    "CACHE_FILTER" => "N",
-    "CACHE_GROUPS" => "Y",
-    "PREVIEW_TRUNCATE_LEN" => "",
-    "ACTIVE_DATE_FORMAT" => "d.m.Y",
-    "SET_TITLE" => "N",
-    "SET_BROWSER_TITLE" => "N",
-    "SET_META_KEYWORDS" => "N",
-    "SET_META_DESCRIPTION" => "N",
-    "SET_LAST_MODIFIED" => "N",
-    "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-    "ADD_SECTIONS_CHAIN" => "N",
-    "HIDE_LINK_WHEN_NO_DETAIL" => "N",
-    "PARENT_SECTION" => "",
-    "PARENT_SECTION_CODE" => "",
-    "INCLUDE_SUBSECTIONS" => "N",
-    "STRICT_SECTION_CHECK" => "N",
-    "DISPLAY_DATE" => "N",
-    "DISPLAY_NAME" => "Y",
-    "DISPLAY_PICTURE" => "Y",
-    "DISPLAY_PREVIEW_TEXT" => "Y",
-    "PAGER_TEMPLATE" => ".default",
-    "DISPLAY_TOP_PAGER" => "N",
-    "DISPLAY_BOTTOM_PAGER" => "N",
-    "PAGER_TITLE" => "Новости",
-    "PAGER_SHOW_ALWAYS" => "N",
-    "PAGER_DESC_NUMBERING" => "N",
-    "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-    "PAGER_SHOW_ALL" => "N",
-    "PAGER_BASE_LINK_ENABLE" => "N",
-    "SET_STATUS_404" => "N",
-    "SHOW_404" => "N",
-    "MESSAGE_404" => ""
-  ),
-  false
-); ?>
+
+
+<?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"template2",
+	Array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "N",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "N",
+		"DISPLAY_DATE" => "N",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array("ID","NAME","PREVIEW_TEXT","PREVIEW_PICTURE","DETAIL_PICTURE",""),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "1",
+		"IBLOCK_TYPE" => "news",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"INCLUDE_SUBSECTIONS" => "N",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(""),
+		"SET_BROWSER_TITLE" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "SORT",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "ASC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N"
+	)
+);?>
 
 
 
@@ -104,7 +93,7 @@ $APPLICATION->SetTitle("1С-Битрикс: Управление сайтом");
 			5 => "TIMESTAMP_X",
 			6 => "",
 		),
-		"SECTION_ID" => $_REQUEST["SECTION_ID"],
+		"SECTION_ID" => "",
 		"SECTION_URL" => "",
 		"SECTION_USER_FIELDS" => array(
 			0 => "UF_COLOR",
@@ -762,7 +751,6 @@ false
 <br>
 <br>
 <br>
-
 
 
 
