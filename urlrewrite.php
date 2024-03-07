00dfa3c930f5a20e808ad386bd92a018f306f08d
Ilya12346 <160493090+Ilya12346@users.noreply.github.com>
2024-03-07 16:53:20 +0300

    works
    works

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,11 +1,27 @@
 <?php
 $arUrlRewrite=array (
+  4 => 
+  array (
+    'CONDITION' => '#^/catalog/([a-zA-Z0-9_-]+)/([a-zA-Z0-9_-]+)/#',
+    'RULE' => 'SECTION_CODE=$1&ELEMENT_CODE=$2',
+    'ID' => '',
+    'PATH' => '/catalog/detail.php',
+    'SORT' => 100,
+  ),
   2 => 
   array (
     'CONDITION' => '#^/news/([a-zA-Z0-9_-]+)/([a-zA-Z0-9_-]+)/#',
     'RULE' => 'SECTION_CODE=$1&ELEMENT_CODE=$2',
     'ID' => '',
     'PATH' => '/news/detail.php',
+    'SORT' => 100,
+  ),
+  3 => 
+  array (
+    'CONDITION' => '#^/catalog/([a-zA-Z0-9_-]+)/#',
+    'RULE' => 'SECTION_CODE=$1',
+    'ID' => '',
+    'PATH' => '/catalog/index.php',
     'SORT' => 100,
   ),
   1 => 
